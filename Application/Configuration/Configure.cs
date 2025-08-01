5f1c26418b78a838da4549b1b93af56b0b2f47d7
dmitry_mih <dmih5678@gmail.com>
2025-08-02 00:18:53 +0300

    chaneges

--- Application/Configuration/Configure.cs
+++ Application/Configuration/Configure.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using Application.Extensions;
-using Application.mappers;
-using Core.Dtos;
-using Core.entities;
-using Core.Interfaces;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
--- photos/Controllers/UserController.cs
+++ photos/Controllers/UserController.cs
@@ -1,57 +1,52 @@
 ﻿using Application.Commands;
-using Application.mappers;
-using Core.Context;
 using Core.Dtos;
 using Core.entities;
-using Core.Exceptions;
-using Core.Interfaces;
-using FluentValidation;
+using Core.Filters;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace photos.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController(IMediator _mediator) : ControllerBase
+    [TypeFilter<LoggingFilter>]
+    public class UserController(IMediator mediator) : ControllerBase
     {
         [HttpPost("Add")]
         public async Task<ActionResult<UserDto>> AddUser([FromBody] UserDto user)
         {
-            var res = await _mediator.Send(new CreateUserCommand(user));            
+            var res = await mediator.Send(new CreateUserCommand(user));            
             return Ok(res); 
 
         }
         [HttpPost("Photo")]
         public async Task<ActionResult<Photo>> UploadPhoto([FromBody]PhotoDto photo,[FromQuery] int userId)
         {
-            var res = await _mediator.Send(new CreatePhotoCommand(photo,userId));
+            var res = await mediator.Send(new CreatePhotoCommand(photo,userId));
             return Ok(res);
         }
 
         [HttpDelete("Delete")]
         public async Task<ActionResult> DeleteUser([FromQuery] int userId)
         {
-
-            await _mediator.Send(new DeleteUserCommand(userId));
-
+            await mediator.Send(new DeleteUserCommand(userId));
             return Ok();
         }
 
         [HttpGet("GetUserWithPhotos")]
         public async Task<ActionResult<UserWithPhoto>> GetUserWithPhotos([FromQuery] int userId)
         {
-
-            var us = await _mediator.Send(new GetUserWithPhotosCommand(userId));
+            var us = await mediator.Send(new GetUserWithPhotosCommand(userId));
             return Ok(us);
 
         }
 
+
+        [ResponseCache(Duration = 300)]
         [HttpGet("Get")]
         public async Task<ActionResult<UserDto>> GetUser([FromQuery] int userId)
         {
-            var us = await _mediator.Send(new GetUserCommand(userId));  
+            var us = await mediator.Send(new GetUserCommand(userId));  
             return Ok(us);
         }
         
